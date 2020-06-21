abe3ee362bb905021a6ca12cd655a1a24fa2fb5d
Kyle Phillips <khillips375@yahoo.com>
2020-06-21 15:38:15 -0400

    Tests that you cant deposit more than the escrow amount

--- test/escrow.js
+++ test/escrow.js
@@ -17,10 +17,24 @@ contract('Escrow', () => {
     before(async () => {
         escrow = await Escrow.deployed();
     })
-    it('Should deposit', async => {
+    it('Should deposit', async () => {
         await escrow.deposit({ from: payer, value: 900 })
         // check balance of ether in smart contract is equal to 900 wei
         const escrowBalance = parseInt(await web3.eth.getBalance(escrow.address));
         assert(escrowBalance === 900)
-    })
+    });
+    it('Should NOT deposit if the sender is not the payer', async () => {
+        assertError(
+            escrow.deposit({ from: accounts[5], value: 100 }),
+            'Sender must be the payer'
+        )
+    });
+    it('Should NOT deposit if transfer exceed amount', async () => {
+        assertError(
+            // Value has to be more than the amount declared in the escrow migration file
+            escrow.deposit({ from: payer, value: 2000 }),
+            'Cant send more than escrow amount'
+
+        )
+    });
 })
