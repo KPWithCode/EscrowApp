ddf245c81248c56ba19a96d3e7115db1caa7c1c2
Kyle Phillips <khillips375@yahoo.com>
2020-06-22 15:30:40 -0400

    All tests passing

--- test/escrow.js
+++ test/escrow.js
@@ -4,14 +4,14 @@ const Escrow = artifacts.require('Escrow');
 const assertError = async (promise, error) => {
     try {
         await promise;
-    } catch(e) {
-        assertError(e.message.includes(error))
+    } catch (e) {
+        assert(e.message.includes(error))
         return;
     }
     assert(false);
 }
 
-contract('Escrow', () => {
+contract('Escrow', accounts => {
     let escrow = null;
     const [lawyer, payer, recipient] = accounts;
     before(async () => {
@@ -38,21 +38,25 @@ contract('Escrow', () => {
     });
     it('Should NOT release funds if full amount has not been received', async () => {
         assertError(
-            escrow.release({ from: lawyer}),
+            escrow.release({ from: lawyer }),
             'cannot release money before the full amount was sent'
         );
     });
     it('Should NOT release funds if the sender is not lawyer', async () => {
-        await escrow.deposit({ from: payer, value: 100});
+        await escrow.deposit({ from: payer, value: 100 });
         assertError(
-            escrow.release({ from: accounts[5]}),
+            escrow.release({ from: accounts[5] }),
             'Only lawyer can release funds'
         );
     });
     it('Should release funds', async () => {
-        const balanceRecipientBefore = web3.utils.toBN(parseInt(await web3.eth.getBalance(recipient)));
+        const balanceRecipientBefore = web3.utils.toBN(
+            await web3.eth.getBalance(recipient)
+        );
         await escrow.release({ from: lawyer })
-        const balanceRecipientAfter = web3.utils.toBN(parseInt(await web3.eth.getBalance(recipient)));
-        assert(balanceRecipientAfter.sub(balanceRecipientBefore).toNumber() === 1000)
+        const balanceRecipientAfter = web3.utils.toBN(
+            await web3.eth.getBalance(recipient)
+        );
+        assert(balanceRecipientAfter.sub(balanceRecipientBefore).toNumber() === 1000);
     })
 })
