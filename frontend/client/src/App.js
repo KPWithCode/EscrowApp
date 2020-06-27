dc16798c633d0f6895333934ad08b0daf7741426
Kyle Phillips <khillips375@yahoo.com>
2020-06-27 02:13:46 -0400

    Basic template at the moment

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [web3,setWeb3] = useState();
+  const [accounts, setAccounts] = useState([]);
   return (
     <div className="App">
-     <h1> It begins</h1>
+     <h1> Escrow</h1>
+
+     <div>
+       <h3> Balance:   <b>wei</b></h3>
+     </div>
+      <div>
+        <form>
+          <div>
+          <label> Deposit</label>
+          <input type="number" placeholder="Deposit"></input>
+          </div>
+          {/* button for form */}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        {/* button to release funds */}
+        <button type="submit">Release</button>
+      </div>
     </div>
   );
 }
