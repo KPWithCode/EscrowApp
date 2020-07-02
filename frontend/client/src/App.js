17e40c460037439d10ad525376c972090c8a1cd5
Kyle Phillips <khillips375@yahoo.com>
2020-07-01 23:48:18 -0400

    Updated

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,34 +1,72 @@
 import React, { useState } from 'react';
+import { getWeb3 } from './utils';
+import Escrow from './contracts/Escrow.json';
+
 import './App.css';
 
-const App = () => {
-  const [web3,setWeb3] = useState();
-  const [accounts, setAccounts] = useState([]);
-  const [contract, setContract] = useState();
-  const [balance, setBalance] = useState();
-  return (
-    <div className="App">
-     <h1> Escrow</h1>
+class App extends Component {
+  state = {
+    web3: undefined,
+    accounts: [],
+    currentAccount: undefined,
+    contract: undefined,
+    balance: undefined
+  }
 
-     <div>
-       <p> Balance:   <b>wei</b></p>
-     </div>
-      <div>
-        <form>
-          <div>
-          <label> Deposit</label>
-          <input type="number" placeholder="Deposit"></input>
-          </div>
-          {/* button for form */}
-          <button type="submit">Submit</button>
-        </form>
+  componentDidMount() {
+    const web3 = await getWeb3();
+    const accounts = web3.eth.getAccounts();
+
+    const networkId = web3.eth.net.getId();
+    const deployedNetwork = Escrow.networks[networkId];
+    const contract = new web3.eth.Contract(
+      Escrow.abi,
+      deployedNetwork && deployedNetwork.address
+    )
+    this.setState({ web3, accounts, contract}, this.updateBalance)
+  }
+  update() {
+    const { contract } = this.state;
+    const balance = await contract.methods.balanceOf().call();
+    this.setState({ balance });
+  }
+  deposit(e) {
+    e.preventDefault();
+    const { contract, accounts } = this.state;
+    await contract.methods.deposit().send({
+      from: accounts[0],
+      value: e.target.elements[0].value
+    });
+  }
+  render() {
+    const { balance } = this.state;
+    if(this.state.web3) {
+      return <div>Loading....</div>
+    }
+    return (
+      <div className="App">
+       <h1> Escrow</h1>
+  
+       <div>
+         <p> Balance:   <b>wei</b></p>
+       </div>
+        <div>
+          <form onSubmit={e => this.deposit(e)}>
+            <div>
+            <label> Deposit</label>
+            <input type="number" placeholder="Deposit"></input>
+            </div>
+            {/* button for form */}
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div>
+          {/* button to release funds */}
+          <button type="submit">Release</button>
+        </div>
       </div>
-      <div>
-        {/* button to release funds */}
-        <button type="submit">Release</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
